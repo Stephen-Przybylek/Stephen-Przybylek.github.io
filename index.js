d304ab3afa63e4305267519b56d4bae616e4292d
Stephen Przybylek <stephen.przybylek@gmail.com>
2020-04-28 19:41:07 -0400

    First batch of updates. Made the layout a bunch of 100% divs and added initial transformations

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 function pageLoad() {
-  var html = document.documentElement;
-  var e = document.getElementById("Header");
-  html.style.setProperty('--opac', 0);
-  e.classList.add("glow");
-
+  var p1 = document.getElementsByClassName("p1")[0];
+  var head = document.getElementById("Name");
+  head.classList.add("glow");
+  p1.style.setProperty('--opac', 1);
 }
 
 function delayedPageLoad() {
-  setTimeout(pageLoad, 1500);
+  setTimeout(pageLoad, 1200);
+}
+
+
+function aboutMeFadeIn() {
+  var x = document.getElementsByClassName("p2")[0].childNodes[3];
+  var y = document.getElementsByClassName("p2")[0].childNodes[5];
+  var z = document.getElementsByClassName("p2")[0].childNodes[7];
+  x.setAttribute("class", "fadein");
+  y.setAttribute("class", "fadein");
+  z.setAttribute("class", "fadein");
 }

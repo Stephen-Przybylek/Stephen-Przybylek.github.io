daf1644b493d1980ba77214a1d41c72678e677a9
Stephen Przybylek <stephen.przybylek@gmail.com>
2020-04-30 18:43:55 -0400

    Redo of the style of the front page and about me page

--- index.js
+++ index.js
@@ -2,8 +2,29 @@ function pageLoad() {
   var p1 = document.getElementsByClassName("p1")[0];
   var head = document.getElementById("Name");
   head.classList.add("glow");
+  setTimeout(programming, 1000);
+  setTimeout(fintech, 2000);
+  setTimeout(algorithms, 3000);
+
   p1.style.setProperty('--opac', 1);
 }
+
+function programming() {
+  var programming = document.getElementById("programming");
+  programming.classList.add("glow");
+}
+
+function fintech() {
+  var fintech = document.getElementById("fintech");
+  fintech.classList.add("glow");
+}
+
+function algorithms() {
+  var algorithms = document.getElementById("algorithms");
+  algorithms.classList.add("glow");
+}
+
+
 
 function delayedPageLoad() {
   setTimeout(pageLoad, 1200);
@@ -14,7 +35,9 @@ function aboutMeFadeIn() {
   var x = document.getElementsByClassName("p2")[0].childNodes[3];
   var y = document.getElementsByClassName("p2")[0].childNodes[5];
   var z = document.getElementsByClassName("p2")[0].childNodes[7];
+  var zz = document.getElementsByClassName("p2")[0].childNodes[9];
   x.setAttribute("class", "fadein");
   y.setAttribute("class", "fadein");
   z.setAttribute("class", "fadein");
+  zz.setAttribute("class", "picfade");
 }
